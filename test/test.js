9cad89a4208866dd78c9a84cdc4cc3d482e8505e
Darko <prijatelce@yahoo.com>
2013-02-17 23:42:07 +0100

    some more functionalities

--- test/test.js
+++ test/test.js
@@ -12,11 +12,24 @@ var app = express();
  * Server starting
  */
 app.get('/', function(req, res){
+	var string='';
     deezer.getUserDetails("2529",function(data) {
     	console.log(data);
     	//res.send(util.inspect(data));
     });
     deezer.getTrackbyID("3135556", function(data){
+    	console.log(data);
+    	//res.send(util.inspect(data));
+    });
+	deezer.getRadioStations(function(data){
+    	console.log(data);
+    	//res.send(util.inspect(data));
+    });
+	deezer.getRadioStation("6", function(data){
+    	console.log(data);
+    	//res.send(util.inspect(data));
+    });
+	deezer.getPlaylist("4341978", function(data){
     	console.log(data);
     	res.send(util.inspect(data));
     });
